3e402f267ceaee53a68151dab03bf26eb61e2911
Sebastian <iulian.mirica2603@gmail.com>
2026-03-14 21:26:11 +0100

    New beginning. Starting to learn correctly so that the game can work properly.

---
+++ core/src/main/java/net/seb234/dandysworld/MainGame.java
@@ -0,0 +1,25 @@
+import com.badlogic.gdx.ApplicationAdapter;
+
+package net.seb234.dandysworld;
+
+
+public class MainGame extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+        // Initialization code here
+        System.out.println("Welcome to Dandy's World!");
+    }
+
+    @Override
+    public void render() {
+        // Game rendering code here
+        System.out.println("Rendering the game...");
+    }
+
+    @Override
+    public void dispose() {
+        // Cleanup resources here
+        System.out.println("Cleaning up & quit");
+    }
+}
--- core/src/main/java/net/seb234/dandysworld/Outfit.java
+++
@@ -1,8 +0,0 @@
-package net.seb234.dandysworld;
-
-public class Outfit {
-
-    public static String defaultTexture() {
-        return "dandy.png";
-    }
-}
